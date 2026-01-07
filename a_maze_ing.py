4986f495753367d8d4664fa752c158197a314cc6
Sxcha <sservant@z2r3p2.42lyon.fr>
2026-01-07 17:01:24 +0100

    sync

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -9,6 +9,21 @@ from utils.mlx_utils import XVar, render_maze_to_mlx
 from utils.maze_utils import generate_maze
 
 
+def regenerate_maze_wrapper(xvar):
+    if xvar.config and xvar.maze is not None:
+        maze = generate_maze(xvar.config, xvar)
+        xvar.maze = maze
+        if xvar.show_path:
+             entry = tuple(xvar.config.ENTRY)
+             exit_p = tuple(xvar.config.EXIT)
+             xvar.path = solve_bfs(maze, entry, exit_p)
+        else:
+             xvar.path = []
+
+        render_maze_to_mlx(xvar.mlx, xvar.mlx_ptr, xvar.win_1, maze,
+                           xvar.config, xvar)
+
+
 def main():
     xvar = XVar()
     try:
@@ -28,12 +43,22 @@ def main():
             xvar.mlx_ptr = xvar.mlx.mlx_init()
             ret, xvar.screen_w, xvar.screen_h = xvar.mlx.mlx_get_screen_size(
                 xvar.mlx_ptr)
-            win_w = min(1920, xvar.screen_w if xvar.screen_w else 1920)
-            win_h = min(1080, xvar.screen_h if xvar.screen_h else 1080)
+            
+            # Adjust window width logic to accommodate UI
+            # We want to use as much space as possible but valid
+            avail_w = xvar.screen_w if xvar.screen_w else 1920
+            avail_h = xvar.screen_h if xvar.screen_h else 1080
+            
+            win_w = min(1920, avail_w)
+            win_h = min(1080, avail_h)
+            
             xvar.win_1 = xvar.mlx.mlx_new_window(xvar.mlx_ptr, win_w, win_h,
                                                  "A-Maze-ing")
             if not xvar.win_1:
                 raise Exception("Can't create MLX window")
+            
+            xvar.regenerate_callback = lambda: regenerate_maze_wrapper(xvar)
+
             xvar.mlx.mlx_key_hook(xvar.win_1, manage_key_simple, xvar)
             xvar.mlx.mlx_hook(xvar.win_1, 33, 0, manage_close, xvar)
             xvar.mlx.mlx_expose_hook(xvar.win_1, manage_expose, xvar)
